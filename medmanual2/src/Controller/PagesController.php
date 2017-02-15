6d994e99df648480754b04306901b56bca8b8069
Jblew <jedrzejblew@gmail.com>
2017-02-15 15:34:52 +0100

    8.3

--- medmanual2/src/Controller/PagesController.php
+++ medmanual2/src/Controller/PagesController.php
@@ -68,7 +68,7 @@ class PagesController extends AppController {
         $page = null;
         if ($id > 0) {
             $page = $this->Pages->get($id, [
-                'contain' => ['User']
+                'contain' => ['Users']
             ]);
         } else {
             $page = $this->Pages->find('all', [
